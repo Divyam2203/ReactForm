bb4f0484ad8f329384b85602090355bb96c95baa
Divyam <divyam2203@gmail.com>
2024-06-08 18:54:54 +0530

    added password show/hide and all fields filled check

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -12,10 +12,22 @@ export default function Form() {
   }
 
   const [selectedCountry, setSelectedCountry] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  let passwordToggle = "password";
+
+  function handleShowPassword() {
+    setShowPassword((editing) => !editing);
+  }
+
+  if (showPassword) {
+    passwordToggle = "text";
+  } else {
+    passwordToggle = "password";
+  }
 
   function handleCountry(event) {
     setSelectedCountry(event.target.value);
-    console.log(selectedCountry);
   }
 
   const cities = CITIES[selectedCountry];
@@ -23,61 +35,143 @@ export default function Form() {
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <label htmlFor="First Name">First Name</label>
+        <label htmlFor="email">Email</label>
         <br />
-        <input type="text" name="firstName" /><span>invalid</span>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className="text-select"
+          required
+        />
       </div>
       <div>
-        <label htmlFor="Second Name">Second Name</label>
-        <br />
-        <input type="text" name="secondName" />
+        <div className="control-row">
+          <div className="control">
+            <label htmlFor="password">Password</label>
+            <br />
+            <input
+              type={passwordToggle}
+              name="password"
+              id="password"
+              className="short-input"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="confirm-password">Confirm Password</label>
+            <br />
+            <input
+              type={passwordToggle}
+              name="confirm-password"
+              id="confirm-password"
+              className="short-input"
+              required
+            />
+          </div>
+        </div>
+        <div className="checkbox-container">
+          <span>
+            <input
+              type="checkbox"
+              className="checkbox"
+              id="checkbox"
+              onClick={handleShowPassword}
+            />
+            Show Password
+          </span>
+        </div>
       </div>
       <div>
-        <label htmlFor="Email">Email</label>
-        <br />
-        <input type="email" name="email" />
+        <div className="control-row">
+          <div>
+            <label htmlFor="firstName">First Name</label>
+            <br />
+            <input
+              type="text"
+              name="firstName"
+              id="firstName"
+              required
+              className="short-input"
+            />
+          </div>
+          <div>
+            <label htmlFor="secondName">Second Name</label>
+            <br />
+            <input
+              type="text"
+              name="secondName"
+              id="secondName"
+              className="short-input"
+              required
+            />
+          </div>
+        </div>
+        <span>invalid</span>
       </div>
-      <div>
-        <label htmlFor="Password">Password</label>
-        <br />
-        <input type="password" name="password" />
+      <div className="control-row">
+        <div>
+          <label htmlFor="country">Country</label>
+          <br />
+          <select
+            name="country"
+            id="country"
+            className="short-input"
+            onChange={handleCountry}
+            required
+          >
+            {COUNTRIES.map((country) => (
+              <option value={country} key={country}>
+                {country}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label htmlFor="city">City</label>
+          <br />
+          <select name="city" id="city" className="short-input" required>
+            {cities.map((city) => (
+              <option value={city} key={city}>
+                {city}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
       <div>
         <label htmlFor="phoneNo">Phone No.</label>
         <br />
-        <input type="tel" name="phoneNo" />
-      </div>
-      <div>
-        <label htmlFor="country">Country</label>
-        <br />
-        <select name="country" onChange={handleCountry}>
-          {COUNTRIES.map((country) => (
-            <option value={country} key={country}>
-              {country}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div>
-        <label htmlFor="city">City</label>
-        <br />
-        <select name="city">
-          {cities.map((city) => (
-            <option value={city} key={city}>
-              {city}
-            </option>
-          ))}
-        </select>
+        <input
+          type="tel"
+          name="phoneNo"
+          id="phoneNo"
+          className="text-select"
+          required
+        />
       </div>
       <div>
         <label htmlFor="panNo">Pan No.</label>
         <br />
-        <input type="text" name="panNo" />
+        <input
+          type="text"
+          name="panNo"
+          id="panNo"
+          className="text-select"
+          required
+        />
       </div>
       <div>
         <label htmlFor="aadhar">Aadhar No.</label>
         <br />
-        <input type="text" name="aadhar" /><span>invalid</span>
+        <input
+          type="text"
+          name="aadhar"
+          id="aadhar"
+          className="text-select"
+          required
+        />
+        <span>invalid</span>
       </div>
 
       <div>
