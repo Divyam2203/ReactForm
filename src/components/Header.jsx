9e5ed48f1067dce68f9dd2106eec98000bc1cacc
Divyam <divyam2203@gmail.com>
2024-06-10 23:22:48 +0530

    app ready to deploy :)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
-import './Header.css'
+import "./Header.css";
+import logo from "../assets/form.png";
 
 export default function Header() {
   return (
     <header>
-      <img src="src\assets\form.png" alt="" />
+      <img src={logo} alt="form logo" />
       <h1>React Form</h1>
     </header>
   );
